8915f98b1686e8e628392b340db49aeeae2482b9
Aleksei <i-ees@ya.ru>
2022-06-30 16:40:30 +1000

    Верстка 4

--- js/script.js
+++ js/script.js
@@ -19,12 +19,11 @@ $(document).ready(function () {
         elem.siblings('.team__photo').removeClass('team__photo--active');
     }
     function openElement(elem) {
+        closeElement($('.team__item .team__name').not(elem));
         elem.addClass('team__name--active');
         elem.siblings('.team__content').addClass('team__content--active');
         elem.siblings('.team__photo').addClass('team__photo--active');
-        closeElement($('.team__item .team__name').not(elem));
     }
-    //    $('.team__content').hide();
     $('.team__name').on('click', function (event) {
         event.preventDefault();
         const $clickTarget = $(this);
